5913517c32f4ab6e67fce7cdabb4e0979b61acb8
Paul Novak <pnovak@alumni.caltech.edu>
2020-04-24 15:49:41 -0700

    Add model for Message class, add relationship with MturkWorker

---
+++ rethinksmoking/orm/message.py
@@ -0,0 +1,18 @@
+from .database import db
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message_content = db.Column(db.String, nullable=False)
+    condition = db.Column(db.String, nullable=False)
+
+    # Foreign key relationship with MturkWorker table
+    mturk_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    mturk_user = db.relationship('MturkWorker', back_populates='messages')
+
+    def __repr__(self):
+        return f'<Message(content={self.message_content})>'
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
