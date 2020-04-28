1af19a99674e2de2fedaad15fb9801aaac1e6aac
Paul Novak <pnovak@alumni.caltech.edu>
2020-04-28 15:32:22 -0700

    Whitespace changes

--- tests/orm/mturk_worker_test.py
+++ tests/orm/mturk_worker_test.py
@@ -23,8 +23,8 @@ class TestMturkWorker:
     def test_post_mturk_worker_with_messages(self, session):
         expected_age = 10
         expected_content = 'Test message'
-        user = MturkWorker(age=expected_age, gender=Gender.Transgender, race='Hispanic and/or Latinx', ethnicity='Unknown',
-                           english_primary_language=True, english_acquisition_age=10,
+        user = MturkWorker(age=expected_age, gender=Gender.Transgender, race='Hispanic and/or Latinx',
+                           ethnicity='Unknown', english_primary_language=True, english_acquisition_age=10,
                            education_level='No schooling completed', income=IncomeLevel.Between25to40, household_size=9,
                            distracted_level=1, seriousness_level=2, ftnd_1=1, ftnd_2=1, ftnd_3=1, ftnd_4=1, ftnd_5=1,
                            ftnd_6=1)
