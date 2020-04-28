a43f84960244ce06d24dc0a71f0585a5f63c8539
Paul Novak <pnovak@alumni.caltech.edu>
2020-04-28 11:03:31 -0700

    Use enums for column data types, where possible

--- tests/orm/mturk_worker_test.py
+++ tests/orm/mturk_worker_test.py
@@ -1,13 +1,13 @@
-from rethinksmoking.orm.mturk_worker import MturkWorker
 from rethinksmoking.orm.message import Message
+from rethinksmoking.orm.mturk_worker import MturkWorker, Gender, IncomeLevel
 
 
 class TestMturkWorker:
     def test_post_mturk_worker(self, session):
         expected_age = 10
-        user = MturkWorker(age=expected_age, gender='Female', race='Hispanic and/or Latinx', ethnicity='Unknown',
+        user = MturkWorker(age=expected_age, gender=Gender.Female, race='Hispanic and/or Latinx', ethnicity='Unknown',
                            english_primary_language=True, english_acquisition_age=10,
-                           education_level='No schooling completed', income='25000', household_size=9,
+                           education_level='No schooling completed', income=IncomeLevel.Below25, household_size=9,
                            distracted_level=1, seriousness_level=2, ftnd_1=1, ftnd_2=1, ftnd_3=1, ftnd_4=1,
                            ftnd_5=1, ftnd_6=1, messages=[])
         user.add()
@@ -23,9 +23,9 @@ class TestMturkWorker:
     def test_post_mturk_worker_with_messages(self, session):
         expected_age = 10
         expected_content = 'Test message'
-        user = MturkWorker(age=expected_age, gender='Female', race='Hispanic and/or Latinx', ethnicity='Unknown',
+        user = MturkWorker(age=expected_age, gender=Gender.Transgender, race='Hispanic and/or Latinx', ethnicity='Unknown',
                            english_primary_language=True, english_acquisition_age=10,
-                           education_level='No schooling completed', income='25000', household_size=9,
+                           education_level='No schooling completed', income=IncomeLevel.Between25to40, household_size=9,
                            distracted_level=1, seriousness_level=2, ftnd_1=1, ftnd_2=1, ftnd_3=1, ftnd_4=1, ftnd_5=1,
                            ftnd_6=1)
         user.messages.append(Message(message_content=expected_content, condition='Unknown'))
