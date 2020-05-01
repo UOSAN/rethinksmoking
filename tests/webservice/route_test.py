293e461e24d87e8567b22c4b7c6b74f4ea1f29e4
Paul Novak <pnovak@alumni.caltech.edu>
2020-05-01 09:12:19 -0700

    Verify mock is called in unit tests, simplify test data

--- tests/webservice/route_test.py
+++ tests/webservice/route_test.py
@@ -18,12 +18,13 @@ class TestPostWorkerRoute:
                 client = app.test_client()
                 response = client.post('/worker', json=bad_worker, content_type='application/json')
                 assert response.status_code == HTTPStatus.OK
+                assert mock_add.called
 
     def test_success(self, app):
         actual_worker = {'age': 10, 'gender': 'Female', 'race': 'Hispanic and/or Latinx', 'ethnicity': 'Unknown',
                          'english_primary_language': True, 'education_level': 'No schooling completed',
                          'income': '25000', 'household_size': 9, 'ftnd_1': 1, 'ftnd_2': 1, 'ftnd_3': 1, 'ftnd_4': 1,
-                         'ftnd_5': 1, 'ftnd_6': 1, 'messages': []}
+                         'ftnd_5': 1, 'ftnd_6': 1}
 
         # Verify 200 OK is returned when a complete object is received
         with mock.patch('rethinksmoking.orm.mturk_worker.MturkWorker.add') as mock_add:
@@ -31,3 +32,4 @@ class TestPostWorkerRoute:
                 client = app.test_client()
                 response = client.post('/worker', json=actual_worker, content_type='application/json')
                 assert response.status_code == HTTPStatus.OK
+                assert mock_add.called
