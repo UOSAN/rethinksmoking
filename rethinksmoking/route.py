2109bcdf831734185251d252ebf94de58b10b02a
Paul Novak <pnovak@alumni.caltech.edu>
2020-05-01 09:10:43 -0700

    Add GET method support for worker route

--- rethinksmoking/route.py
+++ rethinksmoking/route.py
@@ -5,29 +5,34 @@ from flask import (
 from .request_handler import RequestHandler
 from .orm.rating import Rating
 from .orm.score import Score
+from .orm.mturk_worker import MturkWorker
 
 bp = Blueprint('worker', __name__)
 
 
 # Handle POST to the /worker endpoint that contains the required output
-@bp.route('/worker', methods=['POST'])
+@bp.route('/worker', methods=['GET', 'POST'])
 def add_worker_and_messages():
-    if request.is_json:
-        request_output = request.get_json()
-        current_app.logger.info(f'New POST received')
+    if request.method == 'GET':
+        workers = MturkWorker.query.all()
+        return make_response(str(workers), 200)
+    elif request.method == 'POST':
+        if request.is_json:
+            request_output = request.get_json()
+            current_app.logger.info(f'New POST received')
 
-        try:
-            current_app.logger.info(f'POST request as JSON:\n{request_output}\n')
-            handler = RequestHandler(request=request_output)
-            handler.handle_request()
-        except KeyError as ke:
-            current_app.logger.exception(str(ke))
-    else:
-        current_app.logger.info(f'POST request:\n{request.data}\n')
-        return make_response('', 400)
+            try:
+                current_app.logger.info(f'POST request as JSON:\n{request_output}\n')
+                handler = RequestHandler(request=request_output)
+                handler.handle_request()
+            except KeyError as ke:
+                current_app.logger.exception(str(ke))
+        else:
+            current_app.logger.info(f'POST request:\n{request.data}\n')
+            return make_response('', 400)
 
-    # return successfully
-    return make_response('', 200)
+        # return successfully
+        return make_response('', 200)
 
 
 @bp.route('/score', methods=['GET'])
